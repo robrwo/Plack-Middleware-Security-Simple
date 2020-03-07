4f0efc8403c08fd9e58f62e271f6c77dee1f5bf4
Robert Rothenberg <rrwo@cpan.org>
2020-03-07 00:12:56 +0000

    Add files for release

--- lib/Plack/Middleware/Security/Simple.pm
+++ lib/Plack/Middleware/Security/Simple.pm
@@ -1,5 +1,9 @@
 package Plack::Middleware::Security::Simple;
 
+# ABSTRACT: A simple security filter for Plack
+
+use v5.8.0;
+
 use strict;
 use warnings;
 
@@ -13,7 +17,53 @@ use Ref::Util qw/ is_plain_arrayref is_plain_hashref /;
 use Plack::Util;
 use Plack::Util::Accessor qw( rules handler );
 
+# RECOMMEND PREREQ: Ref::Util::XS
+# RECOMMEND PREREQ: Plack::Middleware::HTTPExceptions
+
 our $VERSION = 'v0.2.0';
+
+=head1 SYNOPSIS
+
+  use Plack::Builder;
+
+  builder {
+
+    enable "HTTPExceptions", rethrow => 1;
+
+    enable "Security::Simple",
+        rules => [
+            PATH_INFO => qr{^/cgi-bin/},
+            PATH_INFO => qr{\.(php|asp)$},
+        ];
+
+   ...
+
+  };
+
+=head1 DESCRIPTION
+
+This module provides a simple security filter for PSGI-based
+applications, so that you can filter out obvious exploit-seeking
+scripts.
+
+=attr rules
+
+This is a set of rules. It should be an array-reference or
+L<Hash::Match> object containing matches against keys in the Plack
+environment.
+
+=attr handler
+
+This is a function that is called when a match is found.
+
+It takes the Plack environment as an argument, and returns a
+L<Plack::Response>, or throws an exception for
+L<Plack::Middleware::HTTPExceptions>.
+
+The default handler will log a warning to the C<psgix.logger>, and
+throw a L<HTTP::Exception> for HTTP 400 (Bad Request).
+
+=cut
 
 sub prepare_app {
     my ($self) = @_;
@@ -52,5 +102,15 @@ sub call {
 
     return $self->app->($env);
 }
+
+=head1 SEE ALSO
+
+L<Hash::Match>
+
+L<Plack>
+
+L<PSGI>
+
+=cut
 
 1;
